1c4feac4c8fcb92ab5b3a1952fc4d018a13d8eba
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-05-05 19:53:37 +0200

    fix(derive): update the proc parser
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- rust/lnspec_derive/src/gen_impl.rs
+++ rust/lnspec_derive/src/gen_impl.rs
@@ -1,22 +1,24 @@
 //! Helper module to generate the implementation
 //! for the struct.
+use kproc_parser::proc_macro::TokenStream;
 use kproc_parser::rust::ast_nodes::{AttrToken, StructToken};
 
-pub(crate) fn gen_to_wire_impl(ast_struct: &StructToken) -> String {
+pub(crate) fn gen_to_wire_impl(ast_struct: &StructToken) -> TokenStream {
     let mut code = format!("impl ToWire for {} {{\n", ast_struct.name);
-    code += get_to_wire_fn(ast_struct).as_str();
+    code += &get_to_wire_fn(ast_struct);
     code += "}\n";
-    code
+    // SAFETY: safe to unwrap this because the code generated should be valid rust
+    code.parse().unwrap()
 }
 
 pub(crate) fn get_to_wire_fn(ast_struct: &StructToken) -> String {
-    let mut code = "fn to_wire<W: Write>(&self, buff: &mut W) -> Result<(), IOError> {".to_owned();
+    let mut code = "fn to_wire<W: Write>(&self, buff: &mut W) -> std::io::Result<()> {".to_owned();
     for field in &ast_struct.fields {
         let tmp = if let Some(AttrToken::Attr(value)) = field.attrs.get("msg_type") {
             let attr = value.to_owned().value.unwrap();
             format!(" ({} as u16).to_wire(buff)?;\n", attr)
         } else {
-            format!("   self.{}.to_wire(buff)?;\n", field.name)
+            format!("   self.{}.to_wire(buff)?;\n", field.identifier)
         };
         code += tmp.as_str();
     }
@@ -24,19 +26,46 @@ pub(crate) fn get_to_wire_fn(ast_struct: &StructToken) -> String {
     code
 }
 
-pub(crate) fn gen_from_wire_impl(ast_struct: &StructToken) -> String {
+pub(crate) fn gen_from_wire_impl(ast_struct: &StructToken) -> TokenStream {
     let mut code = format!("impl FromWire for {} {{\n", ast_struct.name);
     code += get_from_wire_fn(ast_struct).as_str();
+    code += "}\n";
+    // SAFETY: this is safe because the code generated should be valid rust
+    code.parse().unwrap()
+}
+
+pub(crate) fn get_from_wire_fn(ast_struct: &StructToken) -> String {
+    let mut code = "fn from_wire<R: Read>(reader: &mut R) -> std::io::Result<Self> {".to_owned();
+    let mut fields = Vec::<String>::new();
+    for field in &ast_struct.fields {
+        code += &format!(
+            "let {} = {}::from_wire(reader);\n",
+            field.identifier, field.ty
+        );
+        fields.push(field.identifier.to_string());
+    }
+    code += &format!("Ok({}::_new({}))", ast_struct.name, fields.join(","));
     code += "}\n";
     code
 }
 
-pub(crate) fn get_from_wire_fn(ast_struct: &StructToken) -> String {
-    let mut code = "fn from_wire<R: Read>(buff: &mut R) -> Self {".to_owned();
-    for field in &ast_struct.fields {
-        code += format!("let {} = {}::from_wire(buff);\n", field.name, field.ty).as_str();
-    }
-    code += "todo!()";
-    code += "}\n";
-    code
+pub(crate) fn generate_from_write_new_method(ast_struct: &StructToken) -> TokenStream {
+    let struct_name = &ast_struct.name;
+    let fields = ast_struct
+        .fields
+        .iter()
+        .map(|field| field.identifier.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+    format!(
+        "
+    impl {struct_name} {{
+         pub(self) fn new({fields}) -> Self {{
+              Self{{ {fields} }}[]
+         }}
+    }}
+"
+    )
+    .parse()
+    .unwrap()
 }
--- rust/lnspec_derive/src/lib.rs
+++ rust/lnspec_derive/src/lib.rs
@@ -1,32 +1,29 @@
 //! Derive crate implement the procedural macros
 //! to implement `FromWire` and `ToWire` in a
 //! struct.
-use gen_impl::{gen_from_wire_impl, gen_to_wire_impl};
-use kproc_parser::kparser::DummyTracer;
-use kproc_parser::kproc_macros::KTokenStream;
-use kproc_parser::proc_macro::TokenStream as TokenStream2;
-use kproc_parser::rust::ast::RustAST;
-use kproc_parser::rust::rust_struct::parse_struct;
 use proc_macro::TokenStream;
 
+use kproc_parser::proc_macro::TokenStream as TokenStream2;
+use kproc_parser::rust::kparser::RustParser;
+
 mod gen_impl;
+use gen_impl::{gen_from_wire_impl, gen_to_wire_impl, generate_from_write_new_method};
 
 #[proc_macro_derive(EncodeWire, attributes(msg_type))]
 pub fn impl_to_wire(tokens: TokenStream) -> TokenStream {
-    let inputv2 = TokenStream2::from(tokens.clone());
-    let mut stream = KTokenStream::new(&inputv2);
-    let tracer = DummyTracer {};
-    let RustAST::Struct(ast) = parse_struct(&mut stream, &tracer);
-    let to_wire_impl = gen_to_wire_impl(&ast);
-    to_wire_impl.parse().unwrap()
+    let mut inputv2 = TokenStream2::from(tokens.clone());
+    let parser = RustParser::new();
+    let struct_ast = parser.parse_struct(&mut inputv2);
+    gen_to_wire_impl(&struct_ast)
 }
 
 #[proc_macro_derive(DecodeWire)]
 pub fn impl_from_wire(tokens: TokenStream) -> TokenStream {
     let inputv2 = TokenStream2::from(tokens.clone());
-    let mut stream = KTokenStream::new(&inputv2);
-    let tracer = DummyTracer {};
-    let RustAST::Struct(ast) = parse_struct(&mut stream, &tracer);
-    let from_wire_impl = gen_from_wire_impl(&ast);
-    from_wire_impl.parse().unwrap()
+    let parser = RustParser::new();
+    let struct_ast = parser.parse_struct(&inputv2);
+    let mut impl_constructor = generate_from_write_new_method(&struct_ast);
+    let from_wire_impl = gen_from_wire_impl(&struct_ast);
+    impl_constructor.extend(from_wire_impl);
+    impl_constructor
 }
