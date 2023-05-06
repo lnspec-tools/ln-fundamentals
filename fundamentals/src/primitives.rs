22db525c5e52260909d85947b5fb61b5184b0209
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-05-06 09:49:49 +0200

    fix: return eos in basic types
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- fundamentals/src/primitives.rs
+++ fundamentals/src/primitives.rs
@@ -41,11 +41,15 @@ macro_rules! to_wire_type {
 
         impl FromWire for $ty {
             fn from_wire<R: Read>(buff: &mut R) -> std::io::Result<Self> {
+                use crate::error;
+
                 const BUFF_SIZE: usize = size_of::<$ty>();
                 let mut tmp_buff = [0; BUFF_SIZE];
                 let size = buff.read(&mut tmp_buff)?;
+                if size == 0 {
+                    return error!("EOS");
+                }
                 let value = <$ty>::from_be_bytes(tmp_buff);
-                assert_eq!(size, size_of::<$ty>());
                 Ok(value)
             }
         }
