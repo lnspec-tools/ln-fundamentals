e3d36e8599bbdd5256beb99e9d186cbe7a4c7959
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-03-27 19:59:14 +0100

    meta: add specs directory
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- fundamentals/src/bolt/bolt1.rs
+++ fundamentals/src/bolt/bolt1.rs
@@ -8,7 +8,6 @@ use crate::prelude::*;
 
 #[derive(DecodeWire, EncodeWire, Debug, Clone)]
 pub struct Error {
-    #[warn(dead_code)]
     #[msg_type = 17]
     pub ty: u16,
     pub channel_id: ChannelId,
@@ -17,7 +16,6 @@ pub struct Error {
 
 #[derive(DecodeWire, EncodeWire, Debug, Clone)]
 pub struct Init {
-    #[warn(dead_code)]
     #[msg_type = 16]
     pub ty: u16,
     pub globalfeatures: BitFlag,
@@ -27,7 +25,6 @@ pub struct Init {
 
 #[derive(DecodeWire, EncodeWire, Debug, Clone)]
 pub struct Ping {
-    #[warn(dead_code)]
     #[msg_type = 18]
     pub ty: u16,
     pub num_pong_bytes: u16,
@@ -36,7 +33,6 @@ pub struct Ping {
 
 #[derive(DecodeWire, EncodeWire, Debug, Clone)]
 pub struct Pong {
-    #[warn(dead_code)]
     #[msg_type = 19]
     pub ty: u16,
     pub ignored: BitFlag,
@@ -44,7 +40,6 @@ pub struct Pong {
 
 #[derive(DecodeWire, EncodeWire, Debug, Clone)]
 pub struct Warning {
-    #[warn(dead_code)]
     #[msg_type = 1]
     pub ty: u16,
     pub channel_id: ChannelId,
